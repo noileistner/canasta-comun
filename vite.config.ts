921019c509033bfd3e58cfd97a0317f61391cd8a
Nói Leistner <noi.leistner@gmail.com>
2023-11-04 09:38:04 +0100

    chore: setup eslint and node version

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import vue from '@vitejs/plugin-vue'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 // Utilities
-import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({ 
+    vue({
       template: { transformAssetUrls }
     }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
